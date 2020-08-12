e9871c22c25a0b40bd972adf164e04f06b28158c
feliperdi <dias.rodrigues.feliphe@gmail.com>
2020-08-12 16:14:14 -0300

    fix conflits

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import logo from '../img/logo-future-eats-invert.png'
+import Login from '../components/Login/Login'
+import {Container, ImgContainer} from './styles/Login'
+
+
+
+export default props =>{
+
+    return <div>Oi</div>;
+
+}
